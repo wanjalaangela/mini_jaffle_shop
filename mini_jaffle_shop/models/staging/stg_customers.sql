743b40d67dac305a8afcd6deef62816f6a93d95d
Angela Katola Wanjala <angelakatolawanjala@gmail.com>
2025-11-07 08:29:45 +0300

    removed CTE in the staging models

--- mini_jaffle_shop/models/staging/stg_customers.sql
+++ mini_jaffle_shop/models/staging/stg_customers.sql
@@ -1,18 +1,7 @@
-
-
 {{ config(materialized='view') }}
 
-with source as (
-    select * from {{ ref('raw_customers') }}
-),
-
-clean as (
-    select
-        id::int as customer_id,
-        trim(first_name)::text as first_name,
-        trim(last_name)::text as last_name
-    from source
-)
-select * from clean
-
-
+select
+  id::int as customer_id,
+  trim(first_name)::text as first_name,
+  trim(last_name)::text as last_name
+from {{ ref('raw_customers') }}
--- mini_jaffle_shop/models/staging/stg_orders.sql
+++ mini_jaffle_shop/models/staging/stg_orders.sql
@@ -1,16 +1,8 @@
-
 {{ config(materialized='view') }}
 
-with source as (
-    select * from {{ ref('raw_orders') }}
-),
-
-clean as (
-    select
-        id::int as order_id,
-        user_id::int as customer_id,
-        order_date::date as order_date,
-        lower(nullif(status, ''))::text as status
-    from source
-)
-select * from clean
+select
+  id::int as order_id,
+  user_id::int as customer_id,
+  order_date::date as order_date,
+  lower(nullif(status, ''))::text as status
+from {{ ref('raw_orders') }}
